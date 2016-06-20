20eccecb11afd4df1ff771157330841915b5bdd2
Nguyen Tuan Sieu <yiivn@outlook.com>
2016-06-21 03:59:43 +0700

    .

--- AdminAssets.php
+++ AdminAssets.php
@@ -9,9 +9,16 @@ use yii\web\AssetBundle;
 class AdminAssets extends AssetBundle
 {
     public $sourcePath = __DIR__.'/assets';
-    public $js = [];
+    public $js = [
+        'js/slimscroll/jquery.slimscroll.min.js',
+        'js/beyond.js',
+    ];
     public $css = [
-        'css/ionicons.min.css',
+        '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300',
+        'css/typicons.min.css',
+        'css/animate.min.css',
+        'css/beyond.css',
+        'css/skins/deepblue.min.css'
     ];
     public $depends = [];
 }
