29c77464d17e8b94e123078472d0633ac1750a0d
Nguyen Tuan Sieu <ideasvn@hotmail.com>
2016-09-23 00:52:04 +0700

    update

--- AdminAssets.php
+++ AdminAssets.php
@@ -10,15 +10,12 @@ class AdminAssets extends AssetBundle
 {
     public $sourcePath = __DIR__.'/assets';
     public $js = [
-        'js/slimscroll/jquery.slimscroll.min.js',
-        'js/beyond.js',
+        'js/app.js',
     ];
     public $css = [
         '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300',
-        'css/typicons.min.css',
-        'css/animate.min.css',
-        'css/beyond.css',
-        'css/skins/deepblue.min.css'
+        'css/AdminLTE.min.css',
+        'css/skins/skin-blue.min.css'
     ];
     public $depends = [];
 }
